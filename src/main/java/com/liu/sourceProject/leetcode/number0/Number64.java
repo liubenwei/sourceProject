426c0849a6e71dcce6d83d11ce33bd861641bc5b
jerryMouse7 <1581933968@qq.com>
2020-07-23 22:08:39 +0800

    add Number64

--- src/main/java/com/liu/sourceProject/leetcode/number0/Number64.java
+++ src/main/java/com/liu/sourceProject/leetcode/number0/Number64.java
@@ -1,4 +1,5 @@
 package com.liu.sourceProject.leetcode.number0;
+
 //给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
 //
 //        说明：每次只能向下或者向右移动一步。
@@ -19,20 +20,20 @@ package com.liu.sourceProject.leetcode.number0;
 //        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 public class Number64 {
     public int minPathSum(int[][] grid) {
-        if(grid == null || grid.length == 0 || grid[0].length == 0 ){
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
             return 0;
         }
         int[][] dp = new int[grid.length][grid[0].length];
         dp[0][0] = grid[0][0];
-        for(int i = 1; i < dp.length; i++){
-            dp[i][0] = grid[i][0]+dp[i-1][0];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
         }
-        for(int j = 1; j < grid[0].length; j++){
-            dp[0][j] = grid[0][j] + dp[0][j-1];
+        for (int j = 1; j < grid[0].length; j++) {
+            dp[0][j] = grid[0][j] + dp[0][j - 1];
         }
-        for(int i = 1; i < dp.length; i++){
-            for(int j = 1; j < dp[0].length; j++){
-                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
                 //System.out.println(dp[i][j]+" "+grid[i][j]);
             }
         }
