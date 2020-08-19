d28e62b28595f2a123b1c57c0b996aab5b7681b2
liubenwei <2544853876@qq.com>
2020-08-19 08:18:08 +0800

    add Number647

---
+++ src/main/java/com/liu/sourceProject/jdk/Source.java
@@ -0,0 +1,13 @@
+package com.liu.sourceProject.jdk;
+
+/**
+ * @author liu
+ * @Date 2020/8/18 20:36
+ */
+public class Source {
+    public static void main(String[] args) {
+        Object object;
+        String string = "fasdf";
+        string.charAt(1);
+    }
+}
--- src/main/java/com/liu/sourceProject/myThreadPool/sender/EmailSender.java
+++ src/main/java/com/liu/sourceProject/myThreadPool/sender/EmailSender.java
@@ -1,8 +1,14 @@
 package com.liu.sourceProject.myThreadPool.sender;
 
+import lombok.Data;
+
 /**
  * @author liu
  * @Date 2020/8/11 19:12
  */
+@Data
 public class EmailSender {
+	public String to;
+	public String from;
+	public String message;
 }
