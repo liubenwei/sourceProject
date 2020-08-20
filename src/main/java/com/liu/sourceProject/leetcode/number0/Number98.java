67fe1008f7cb9979220e9b40048ec7da41892373
liubenwei <2544853876@qq.com>
2020-08-20 20:41:32 +0800

    add Number529

---
+++ src/main/java/com/liu/sourceProject/leetcode/number0/Number98.java
@@ -0,0 +1,14 @@
+package com.liu.sourceProject.leetcode.number0;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * @author liu
+ * @Date 2020/8/19 16:36
+ *
+ */
+public class Number98 {
+    public boolean isValidBST(TreeNode root) {
+        return false;
+    }
+}
