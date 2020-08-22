0efc780bb86dd5827d56845d073c80d60ce7ef92
liubenwei <2544853876@qq.com>
2020-08-22 15:32:40 +0800

    add Number701

--- src/main/java/com/liu/sourceProject/leetcode/TreeNode.java
+++ src/main/java/com/liu/sourceProject/leetcode/TreeNode.java
@@ -4,8 +4,11 @@ import lombok.Data;
 
 @Data
 public class TreeNode {
-      public    int val;
-      public TreeNode left;
-      public TreeNode right;
-     TreeNode(int x) { val = x; }
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+
+	public TreeNode(int x) {
+		val = x;
+	}
 }
