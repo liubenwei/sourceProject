91fc12fe8134a051ed669760a5d8c9b9ba3c6236
liubenwei <2544853876@qq.com>
2020-08-31 20:17:03 +0800

    add Number841

--- src/main/java/com/liu/sourceProject/leetcode/number800/Number841.java
+++ src/main/java/com/liu/sourceProject/leetcode/number800/Number841.java
@@ -33,6 +33,7 @@ import java.util.Set;
  *       著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Number841 {
+
 	Set<Integer> set;
 
 	public boolean canVisitAllRooms(List<List<Integer>> rooms) {
