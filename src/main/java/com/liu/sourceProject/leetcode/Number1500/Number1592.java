f2cd7ce12b78e6b8b27e863dea16f3d40d7e86fe
liubenwei <2544853876@qq.com>
2020-09-27 08:09:53 +0800

    add Number235

---
+++ src/main/java/com/liu/sourceProject/leetcode/Number1500/Number1592.java
@@ -0,0 +1,9 @@
+package com.liu.sourceProject.leetcode.Number1500;
+
+/**
+ * @author liu
+ * @Date 2020/9/26 15:39
+ */
+public class Number1592 {
+
+}
---
+++ src/main/java/com/liu/sourceProject/leetcode/number100/Number106.java
@@ -0,0 +1,8 @@
+package com.liu.sourceProject.leetcode.number100;
+
+/**
+ * @author liu
+ * @Date 2020/9/26 15:38
+ */
+public class Number106 {
+}
