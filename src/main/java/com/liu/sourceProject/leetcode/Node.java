d829647738505fa62547137a360f8504eb5b3b76
liubenwei <2544853876@qq.com>
2020-09-28 08:40:59 +0800

    add Number117

---
+++ src/main/java/com/liu/sourceProject/leetcode/Node.java
@@ -0,0 +1,16 @@
+package com.liu.sourceProject.leetcode;
+
+/**
+ * @author liu
+ * @Date 2020/9/28 8:28
+ */
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node(int x) {
+        val = x;
+    }
+}
--- src/main/java/com/liu/sourceProject/leetcode/Number1500/Number1592.java
+++ src/main/java/com/liu/sourceProject/leetcode/Number1500/Number1592.java
@@ -1,9 +1,10 @@
 package com.liu.sourceProject.leetcode.Number1500;
 
+import org.apache.ibatis.session.SqlSession;
+
 /**
  * @author liu
  * @Date 2020/9/26 15:39
  */
 public class Number1592 {
-
 }
