b00d629441b797062f94cdf113d097ad674d8a1f
liubenwei <2544853876@qq.com>
2020-10-29 14:33:54 +0800

    update DecoratorPattern

--- src/main/java/com/liu/sourceProject/jdk/reflection/ReflectionTest.java
+++ src/main/java/com/liu/sourceProject/jdk/reflection/ReflectionTest.java
@@ -1,11 +1,24 @@
 package com.liu.sourceProject.jdk.reflection;
 
+import java.lang.reflect.Field;
+
 public class ReflectionTest {
-	public Boolean isDiffentObject(Object oldObject, Object newObject) {
+	public Boolean isDiffentObject(Object oldObject, Object newObject) throws IllegalAccessException {
 		Class<?> oldObjectClazz = oldObject.getClass();
 		Class<?> newObjectClazz = newObject.getClass();
-		if(!oldObjectClazz.equals(newObjectClazz)){
+		if(oldObjectClazz.equals(newObjectClazz)){
+			Field[] oldDeclaredFields = oldObjectClazz.getDeclaredFields();
+			for(Field field : oldDeclaredFields){
+				field.setAccessible(true);
+				Object oldValue = field.get(oldObject);
+				Object newValue = field.get(newObject);
+				if( (oldValue == null && newValue!= null) || (oldValue != null && !oldValue.equals(newValue))){
+					return false;
+				}
+			}
 
+		}else{
+			return false;
 		}
 
 		return true;
