71408fd8ec3936a105d51c82a5e95fa174419db2
liubenwei <2544853876@qq.com>
2020-11-29 09:11:31 +0800

    add Number976

---
+++ src/main/java/com/liu/sourceProject/leetcode/number900/Number976.java
@@ -0,0 +1,53 @@
+package com.liu.sourceProject.leetcode.number900;
+
+import java.util.Arrays;
+
+/**
+ * @author liu
+ * @Date 2020/11/29 9:11
+ * 给定由一些正数（代表长度）组成的数组 A，返回由其中三个长度组成的、面积不为零的三角形的最大周长。
+ *
+ * 如果不能形成任何面积不为零的三角形，返回 0。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：[2,1,2]
+ * 输出：5
+ * 示例 2：
+ *
+ * 输入：[1,2,1]
+ * 输出：0
+ * 示例 3：
+ *
+ * 输入：[3,2,3,4]
+ * 输出：10
+ * 示例 4：
+ *
+ * 输入：[3,6,2,3]
+ * 输出：8
+ *  
+ *
+ * 提示：
+ *
+ * 3 <= A.length <= 10000
+ * 1 <= A[i] <= 10^6
+ * 通过次数24,621提交次数43,274
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/largest-perimeter-triangle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Number976 {
+    public int largestPerimeter(int[] A) {
+        Arrays.sort(A);
+        int length = A.length;
+        for(int i = length - 3; i >= 0; i--){
+            if(A[i] + A[i+1] > A[i + 2]){
+                return A[i] + A[i+1] +A[i+2];
+            }
+        }
+        return 0;
+    }
+}
