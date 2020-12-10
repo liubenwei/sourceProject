206db70724f4b15522920933f23ea25386886e22
liubenwei <2544853876@qq.com>
2020-12-10 09:08:07 +0800

    add Number860

---
+++ src/main/java/com/liu/sourceProject/leetcode/number800/Number860.java
@@ -0,0 +1,41 @@
+package com.liu.sourceProject.leetcode.number800;
+
+/**
+ * @author liu
+ * @Date 2020/12/10 8:51
+ */
+public class Number860 {
+    public static void main(String[] args) {
+        boolean b = new Number860().lemonadeChange(new int[]{5,5,5,5,10,5,10,10,10,20});
+        System.out.println(b);
+    }
+    public boolean lemonadeChange(int[] bills) {
+        int fiveCount = 0, tenCount = 0;
+        for(int bill : bills){
+            if(bill == 5){
+                fiveCount++;
+            }else if(bill == 10){
+                if(fiveCount > 0){
+                    fiveCount--;
+                    tenCount++;
+                }else{
+                    return false;
+                }
+            }else {
+                int temp = 15;
+                while(temp > 10 && tenCount != 0){
+                    temp -= 10;
+                    tenCount--;
+                }
+                while(temp != 0 && fiveCount > 0){
+                    temp -= 5;
+                    fiveCount--;
+                }
+                if(temp != 0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
