6335750220c30aa64f9cfeb2ee311aebab859fc3
liuwei <2544853876@qq.com>
2021-03-05 18:40:34 +0800

    add Number

---
+++ src/main/java/com/liu/sourceProject/leetcode/number200/Number232.java
@@ -0,0 +1,46 @@
+package com.liu.sourceProject.leetcode.number200;
+
+import java.util.Stack;
+
+public class Number232 {
+}
+
+class MyQueue {
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+    /** Initialize your data structure here. */
+    public MyQueue() {
+
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
