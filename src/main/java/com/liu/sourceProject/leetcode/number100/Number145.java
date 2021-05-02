fd88d6978ec3deb1e1c81a2434b489ace6f74383
liubenwei <2544853876@qq.com>
2021-05-02 17:29:42 +0800

    q去除部分无用的代码

--- src/main/java/com/liu/sourceProject/leetcode/number100/Number145.java
+++ src/main/java/com/liu/sourceProject/leetcode/number100/Number145.java
@@ -1,8 +1,9 @@
 package com.liu.sourceProject.leetcode.number100;
 
-import com.sun.org.apache.bcel.internal.generic.RETURN;
-
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
 /**
  * @author liu
@@ -10,35 +11,37 @@ import java.util.*;
  */
 public class Number145 {
 
-	public List<Integer> postorderTraversal(TreeNode root) {
-		List<Integer> res = new ArrayList<>();
-		Deque<TreeNode> nodeStack = new ArrayDeque<>();
-		TreeNode current = root;
-		TreeNode pre = null;
-		while (!nodeStack.isEmpty() || current != null) {
-			while (current != null) {
-				nodeStack.push(current);
-				current = current.left;
-			}
-			current = nodeStack.peek();
-			if (current.right == null || current.right == pre) {
-				res.add(current.val);
-				pre = current;
-				nodeStack.pop();
-				current = null;
-			} else {
-				current = current.right;
-			}
-		}
-		return res;
-	}
-	List<Integer> res = new ArrayList<>();
-	public List<Integer> postorderTraversal1(TreeNode root) {
-		if (root != null) {
-			postorderTraversal1(root.left);
-			postorderTraversal1(root.right);
-			res.add(root.val);
-		}
-		return res;
-	}
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> nodeStack = new ArrayDeque<>();
+        TreeNode current = root;
+        TreeNode pre = null;
+        while (!nodeStack.isEmpty() || current != null) {
+            while (current != null) {
+                nodeStack.push(current);
+                current = current.left;
+            }
+            current = nodeStack.peek();
+            if (current.right == null || current.right == pre) {
+                res.add(current.val);
+                pre = current;
+                nodeStack.pop();
+                current = null;
+            } else {
+                current = current.right;
+            }
+        }
+        return res;
+    }
+
+    public List<Integer> postorderTraversal1(TreeNode root) {
+        if (root != null) {
+            postorderTraversal1(root.left);
+            postorderTraversal1(root.right);
+            res.add(root.val);
+        }
+        return res;
+    }
 }
